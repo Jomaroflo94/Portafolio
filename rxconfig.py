de392a616aca8c6f83e1fac325b9c5957a2b4d27
Jomaroflo94 <jomaroflo94@hotmail.com>
2024-11-29 09:53:05 +0100

    Change RxConfig

--- rxconfig.py
+++ rxconfig.py
@@ -1,7 +1,7 @@
 import reflex as rx
 
 class Config(rx.Config):
-    app_name = "portafolio"
+    app_name = "Portafolio"
     deploy_backend = False,
     deploy_url = "https://Jomaroflo94.github.io/Portafolio"
 
