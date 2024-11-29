d2de461baabe3720e928d4de22792045568beae9
Jomaroflo94 <jomaroflo94@hotmail.com>
2024-11-29 10:14:03 +0100

    Try update config to define relative paths in deploy

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,6 @@ import reflex as rx
 class Config(rx.Config):
     app_name = "Portafolio"
     deploy_backend = False,
-    deploy_url = "https://Jomaroflo94.github.io/Portafolio"
+    deploy_url = ""
 
 config = Config()
