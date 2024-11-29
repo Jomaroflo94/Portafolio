82f09226c4719c6b2ed52959541a38a8d248d891
Jomaroflo94 <jomaroflo94@hotmail.com>
2024-11-29 12:46:04 +0100

    Change README and added toggle theme (disabled) for display dark theme by default

--- portafolio/views/footer/footer.py
+++ portafolio/views/footer/footer.py
@@ -12,6 +12,7 @@ def footer(profile, media_list, is_mobile: bool) -> rx.Component:
         rx.flex(
             footer_media(profile['name'], media_list, is_mobile),
             footer_last_update(profile['last_update'], is_mobile),
+            dark_mode_toggle(),
             spacing=Size.DEFAULT.value,
             margin_top=EmSize.SMALL.value if is_mobile else EmSize.DEFAULT.value,
             flex_direction=["column", "row"]
@@ -33,37 +34,32 @@ def footer_media(name: str, media_list, is_mobile: bool)-> rx.Component:
     )
 
 def footer_last_update(last_update: str, is_mobile: bool)-> rx.Component:
-    return rx.hstack(
-        rx.vstack(
-            heading(
-                "Última actualización:",
-                as_="h3",
-                size=Size.X_SMALL if is_mobile else Size.SMALL
-            ),
-            rx.text(
-                last_update,
-                size=Size.X_SMALL.value if is_mobile else Size.SMALL.value
-            ),
-            align="end" if not is_mobile else "start",
-            spacing=Size.XX_SMALL.value if is_mobile else Size.X_SMALL.value
+    return rx.vstack(
+        heading(
+            "Última actualización:",
+            as_="h3",
+            size=Size.X_SMALL if is_mobile else Size.SMALL
         ),
-        # dark_mode_toggle(),
-        spacing=Size.XX_SMALL.value if is_mobile else Size.X_SMALL.value,
-        align="end" if not is_mobile else "start"
+        rx.text(
+            last_update,
+            size=Size.X_SMALL.value if is_mobile else Size.SMALL.value
+        ),
+        align="end" if not is_mobile else "start",
+        spacing=Size.XX_SMALL.value if is_mobile else Size.X_SMALL.value
     )
 
 def dark_mode_toggle() -> rx.Component:
-    return rx.segmented_control.root(
-        rx.segmented_control.item(
-            rx.icon(tag="sun", size=20),
-            value="light",
+    return rx.vstack(
+        rx.segmented_control.root(
+            rx.segmented_control.item(
+                rx.icon(tag="moon", size=20),
+                value="dark",
+            ),
+            on_change=set_color_mode,
+            variant="surface",
+            radius="large",
+            value=color_mode,
+            default_value="dark"
         ),
-        rx.segmented_control.item(
-            rx.icon(tag="moon", size=20),
-            value="dark",
-        ),
-        on_change=set_color_mode,
-        variant="surface",
-        radius="large",
-        value=color_mode,
+        display="none"
     )
